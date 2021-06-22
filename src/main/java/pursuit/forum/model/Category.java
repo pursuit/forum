0138ca9642f6b69054e9c5eddc1deea2e81da120
@danielbintar <daniel.bintar@bukalapak.com>
2021-06-22 22:15:03 +0700

    created at and customm name

--- src/main/java/pursuit/forum/model/Category.java
+++ src/main/java/pursuit/forum/model/Category.java
@@ -1,11 +1,15 @@
 package pursuit.forum.model;
 
 import java.util.Objects;
+import java.time.Instant;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = "categories")
@@ -13,16 +17,13 @@ public class Category {
 
     @Id
     public Integer id;
+
+    @JsonProperty("parent_id")
     public Integer parentId;
+
     public String name;
     public String description;
 
-    public Category() {
-    }
-
-    public Category(Integer id, Integer parentId, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
+    @JsonProperty("created_at")
+    public Instant createdAt;
 }
